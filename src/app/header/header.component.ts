42fa6c301d2f226185d1872fa4986266f663a03e
Marwen Hanzouli <marwenhanzouli@gmail.com>
2020-06-18 12:59:58 +0100

    update in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavigationService } from '../services/navigation.service';
+import { LanguesService, Langue } from '../services/langues.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,16 +10,34 @@ import { NavigationService } from '../services/navigation.service';
 export class HeaderComponent implements OnInit {
 
   page:string;
+  public languages:Langue[];
+  selectedLanguage:string="FR";
   constructor(private router:Router,
-              private navigationService:NavigationService) { }
+              private navigationService:NavigationService,
+              private languesService:LanguesService) { }
 
   ngOnInit() { 
     this.navigationService.subject.subscribe((data)=>{
       this.page=data;
-    })
+    });
+    this.languesService.subject.subscribe((data)=>{
+      this.languages=data;
+    });
   }
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.page=this.router.url.split('/home/')[1];
+  }
+  changeLangue(langue){
+    
+    this.languages=this.languages.map((l)=>{
+      if(l.langue===langue){
+        l.selected=true;
+        return l;
+      }
+      l.selected=false;
+      return l;
+    });
+    this.languesService.changeLanguage(this.languages);
   }
 }
